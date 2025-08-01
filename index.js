5712d78f11fd587e4be7b692318248a8c4b096db
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2025-08-01 19:04:09 -0300

    :sparkles: feat: MVP - start CLI implementation
    	- CLI run! No problems!

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import {NlpManager} from 'node-nlp';
+import {Command} from 'commander';
+import chalk from 'chalk';
+
+const program = new Command();
+
+program
+  .name('sentimento')
+  .description('Análise de sentimento em português')
+  .argument('<frase>', 'Frase a ser analisada')
+  .action((frase) => {
+    const manager = new NlpManager({ languages: ['pt'] });
+    manager.addDocument('pt', frase, 'sentimento.analisar');
+    manager.train().then(() => {
+      manager.process('pt', frase).then((result) => {
+        console.log(chalk.green('Resultado da análise de sentimento:'));
+        console.log(result);
+      });
+    });
+  });
+
+  program.parse();
